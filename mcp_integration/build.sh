89e414e5e95a79b59b600187c3ff218eddc73ef7
ashish.kumar85297 <ashish.kumar85297@gmail.com>
2025-09-06 05:37:30 +0530

    .

--- mcp_integration/build.sh
+++ mcp_integration/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Change to the mcp_integration directory
+cd "$(dirname "$0")"
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
